fc70fe17170a58faf8ab1924e526498fdfbf72bd
russdeb <70952282+russdeb@users.noreply.github.com>
2021-02-01 09:16:13 +0100

    Update uppgift.js

--- uppgift.js
+++ uppgift.js
@@ -3,26 +3,30 @@ var span = document.getElementsByTagName('span');
 var card = document.getElementsByClassName('card');
 var l = 0;
 window.onload = function(){
+    var base = card[0].offsetWidth;
 span[1].onclick = ()=>{
+    console.log(base)
     l++;
     for(var i of card)
     {
         if (l==0) {i.style.left = "0px";}
-        if (l==1) {i.style.left = "-740px";}
-        if (l==2) {i.style.left = "-1480px";}
-        if (l==3) {i.style.left = "-2220px";}
-        if (l==4) {i.style.left = "-2960px";}
+        if (l==1) {i.style.left = -base + "px";}
+        if (l==2) {i.style.left = -2*base + "px";}
+        if (l==3) {i.style.left = -3*base + "px";}
+        if (l==4) {i.style.left = -4*base + "px";}
         if (l>4) {l=4;}
     }
 }
 span[0].onclick = ()=>{
+    console.log(1);
     l--;
     for(var i of card)
     {
         if (l==0) {i.style.left = "0px";}
-        if (l==1) {i.style.left = "-740px";}
-        if (l==2) {i.style.left = "-1480px";}
-        if (l==3) {i.style.left = "-2220px";}
+        if (l==1) {i.style.left = -base + "px";}
+        if (l==2) {i.style.left = -2*base + "px";}
+        if (l==3) {i.style.left = -3*base + "px";}
+        if (l==4) {i.style.left = -4*base + "px";}
         if (l<0) {l=0;}
     }
 }
